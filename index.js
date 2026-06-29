d8bfc444a0cc8cb248c3b32aaca8d01fee1136cf
MarcDevGuy127 <marceloh.github@gmail.com>
2026-06-28 22:10:06 -0300

    api: add get and post methods

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 const express = require('express'); // here is part of the middleware  
+const req = require('express/lib/request');
 const app = express(); //  it's a function
 const PORT = 8080;
 
-app.use(express.json()); // here is the middleware
+app.use(express.json()); // here is the middleware to read JSON
 
+// database
+let produtos = [
+    { id: 1, nome: 'Teclado Mecânico'},
+    { id: 2, nome: 'Mouse Gamer'}
+];
+
+// GET - Return the products list
+app.get('/produtos', (req, res) => { // that's the route
+    res.status(200).json(produtos);
+});
+
+// POST - Add new product
+app.post('/produtos', (req, res) => {
+    const novoProduto = {
+        id: produtos.length +1,
+        nome: req.body.nome
+    };
+
+    produtos.push(novoProduto);
+    res.status(201).json(novoProduto);
+});
+
+
+/*
 // adding get endpoint
 app.get('/cars', (req, res) => { // that's the route
     res.status(200).send({
@@ -12,6 +37,13 @@ app.get('/cars', (req, res) => { // that's the route
         year: '2014'
     }) 
 });
+
+app.post('/cars', (req, res)) => {
+    const newCar = {
+        name: req.body.name  
+    };
+}
+
 
 // adding post endpoint
 app.post('/cars/:id', (req, res) => { // using route params for every car have a id
@@ -26,7 +58,7 @@ app.post('/cars/:id', (req, res) => { // using route params for every car have a
         cars: `car with your ${image} and ID of ${id}`,
     });
 });
-
+*/
 app.listen(PORT, () => {
-    console.log(`It's alive on http://localhost:${PORT}`);
+    console.log(`API rodando com sucesso em: http://localhost:${PORT}`);
 });
